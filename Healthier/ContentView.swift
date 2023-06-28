d97f715fc0b3f48a57c8ac8dccc353611e3a0a2a
Eyuel Moges <e4eyti@gmail.com>
2023-06-28 13:19:44 -0700

    Initial Commit

---
+++ Healthier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Healthier
+//
+//  Created by Joel on 28/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Healthier/HealthierApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthierApp.swift
+//  Healthier
+//
+//  Created by Joel on 28/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct HealthierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
